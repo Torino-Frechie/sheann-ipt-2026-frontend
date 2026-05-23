a70764c31e079bccd99e068db2487dc4c20244fd
Frechie Ann Torino <frechieannt@gmail.com>
2026-05-23 17:26:43 +0800

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     // TODO: Replace with your actual deployed Vercel backend URL
-    apiUrl: ''
+    apiUrl: 'sheann-ipt-2026-backend.vercel.app'
 };
